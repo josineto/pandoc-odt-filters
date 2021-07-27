e3ca9774639bf44a88efd3832ac35170c6f9f46b
José de Mattos Neto <josepmneto@gmail.com>
2021-07-26 21:50:20 -0300

    test: refactoring using a defaults file

--- test/run-test.sh
+++ test/run-test.sh
@@ -7,52 +7,11 @@ cd ..
 
 echo Generating $odt
 
-pandoc -s $file \
-  -M bibliography=test/bibliography.yaml \
-  -M csl=test/abnt.csl \
-  -M numberSections=true \
-  -M autoSectionLabels=true \
-  -M linkReferences=true \
-  -M nameInLink=true \
-  --filter pandoc-crossref \
-  --filter pandoc-citeproc \
-  --lua-filter abstract.lua \
-  --lua-filter odt-anchors.lua \
-  --lua-filter odt-bib-style.lua \
-  --lua-filter odt-captions.lua \
-  --lua-filter odt-lists.lua \
-  --lua-filter odt-smallcaps.lua \
-  --lua-filter odt-custom-styles.lua \
-  --resource-path=test \
-  --reference-doc=test/custom-reference-doc.odt \
-  -p \
-  -o $odt \
-  -f markdown+smart \
-  -t odt
+pandoc $file -d test/test -t odt -o $odt
 
 echo Generating $html
 
-pandoc -s $file \
-  -M bibliography=test/bibliography.yaml \
-  -M csl=test/abnt.csl \
-  -M numberSections=true \
-  -M autoSectionLabels=true \
-  -M linkReferences=true \
-  -M nameInLink=true \
-  --filter pandoc-crossref \
-  --filter pandoc-citeproc \
-  --lua-filter abstract.lua \
-  --lua-filter odt-anchors.lua \
-  --lua-filter odt-bib-style.lua \
-  --lua-filter odt-captions.lua \
-  --lua-filter odt-lists.lua \
-  --lua-filter odt-smallcaps.lua \
-  --lua-filter odt-custom-styles.lua \
-  --resource-path=test \
-  -p \
-  -o $html \
-  -f markdown+smart \
-  -t html5
+pandoc $file -d test/test -t html5 -o $html
 
 echo Open $odt and $html. Just $odt should be affected by odt- like filters.
 
